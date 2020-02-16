e7cfefed35dd0ca1c0bb9ce9e691c5c2668ae743
Andrey <andreynrosa@gmail.com>
2020-02-16 17:19:35 -0300

    add cors

--- api/node/src/server.js
+++ api/node/src/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const routes = require("./routes/routes");
+var cors = require('cors')
 const  bodyParser = require('body-parser')
+
 require("./database");
 
 const app = express();
-
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
